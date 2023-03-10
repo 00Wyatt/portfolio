2f4199036ef72092bdc8e402a2e314e96a7781d9
Wyatt Channings <wyattchannings@gmail.com>
2023-03-10 09:14:38 +0800

    Update contact form

--- src/partials/Contact.js
+++ src/partials/Contact.js
@@ -19,7 +19,7 @@ export default function Contact() {
     event.preventDefault();
     console.log(inputs)
     axios.defaults.headers.post['Content-Type'] = 'application/json';
-    axios.post('https://formsubmit.co/ajax/3c68556da94d174965e6408ddc34e238', {
+    axios.post('https://formsubmit.co/ajax/wyattchannings+site@gmail.com', {
       inputs
     })
       .then((response) => {
