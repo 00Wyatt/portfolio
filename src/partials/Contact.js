c24410e6c285d8f6cfda696294ea9aee7e673439
Wyatt Channings <wyattchannings@gmail.com>
2023-03-26 16:43:06 +0800

    Use Netlify to handle Contact form

--- src/partials/Contact.js
+++ src/partials/Contact.js
@@ -1,6 +1,5 @@
 import { useState } from "react";
 import { motion } from "framer-motion";
-// import axios from 'axios';
 import variants from "../components/FramerVariants";
 import Shapes from "../components/Shapes";
 
@@ -18,17 +17,15 @@ export default function Contact() {
   const handleSubmit = (event) => {
     event.preventDefault();
     console.log(inputs)
-    fetch("https://formsubmit.co/ajax/wyattchannings+portfolio@gmail.com", {
+
+    const contactForm = event.target;
+    const formData = new FormData(contactForm);
+
+    fetch("/", {
       method: "POST",
-      headers: {
-        'Content-Type': 'application/json',
-        'Accept': 'application/json'
-      },
-      body: JSON.stringify({
-        inputs
-      })
+      headers: { "Content-Type": "application/x-www-form-urlencoded" },
+      body: new URLSearchParams({ "form-name": "contact", formData }).toString(),
     })
-      .then(response => response.json())
       .then((data) => {
         console.log(data)
         alert("Message received. Thank you!")
@@ -38,18 +35,6 @@ export default function Contact() {
         alert("Something went wrong while sending your message. Please try again later or contact me on LinkedIn or by email.");
       });
 
-    // axios.defaults.headers.post['Content-Type'] = 'application/json';
-    // axios.post('https://formsubmit.co/ajax/3c68556da94d174965e6408ddc34e238', {
-    //   inputs
-    // })
-    //   .then((response) => {
-    //     console.log(response);
-    //     alert("Message received. Thank you!");
-    //   })
-    //   .catch((error) => {
-    //     console.error(error);
-    //     alert("Something went wrong while sending your message. Please try again later or contact me on LinkedIn or by email.");
-    //   });
     setInputs({})
   }
 
@@ -68,12 +53,6 @@ export default function Contact() {
             <p>Have a question or want to work together?</p>
           </div>
           <form onSubmit={handleSubmit}>
-            {/* Honeypot */}
-            <input type="text" name="_honey" style={{ display: "none" }} />
-            {/* Disable reCAPTCHA */}
-            <input type="hidden" name="_captcha" value="false" />
-            {/* Email template */}
-            <input type="hidden" name="_template" value="box" />
             <div className="field">
               <label htmlFor="name">Name</label>
               <input type="text" id="name" name="name" value={inputs.name || ""}
